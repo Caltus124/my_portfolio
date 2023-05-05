e5f2f904253bb1bc99eea21550c3e5ec0bbbdf2e
kali <kali@0x0000000000>
2023-05-05 16:44:58 +0200

    projets

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
@@ -16,7 +16,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/rdp', name: 'app_rdp')]
+    #[Route('/', name: 'app_rdp')]
     public function index_rdp(): Response
     {
         return $this->render('home/rdp.html.twig', [
